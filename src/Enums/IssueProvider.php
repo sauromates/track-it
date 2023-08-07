e5591351e84c84c80c69d46f33da06884b166852
sauromates <vs.girenko@mail.ru>
2023-08-07 09:40:27 +0300

    issue providers enum

---
+++ src/Enums/IssueProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Sauromates\TrackIt\Enums;
+
+enum IssueProvider: string
+{
+    case LOCAL = 'local';
+
+    // Examples:
+    // case REDMINE = 'redmine';
+    // case GITLAB = 'gitlab';
+}
